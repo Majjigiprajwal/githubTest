618d302f422ec9ab9be5b178a4902637a2d1d499
majjigiprajwal <prajwal1majjigi@gmail.com>
2023-08-11 15:16:51 +0530

    commited again

--- index.js
+++ index.js
@@ -5,9 +5,22 @@
 // console.log(document.title);
 
 var lists = document.getElementsByClassName('list-group-item');
-console.log(lists);
+
 
 lists[2].style.background="green";
-for(let i=0;lists.length;i++){
+for(let i=0;i<lists.length;i++){
     lists[i].style.fontWeight="bold";
 }
+
+var newList = document.createElement('li');
+var mainList = document.getElementById('items');
+mainList.appendChild(newList);
+newList.classList="new-list";
+newList.style.listStyle="none";
+newList.style.width="auto";
+newList.style.height="50px";
+newList.style.marginTop="5px";
+newList.style.border="0.5px solid grey";
+
+var newLists = document.getElementsByTagName('li');
+newLists[4].style.background="yellow";
