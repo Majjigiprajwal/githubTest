d5d92e14578add0ae3496e0e1d00e7b8cd104430
majjigiprajwal <prajwal1majjigi@gmail.com>
2023-08-11 15:02:33 +0530

    new commit

--- index.js
+++ index.js
@@ -4,3 +4,10 @@
 // console.log(document.body);
 // console.log(document.title);
 
+var lists = document.getElementsByClassName('list-group-item');
+console.log(lists);
+
+lists[2].style.background="green";
+for(let i=0;lists.length;i++){
+    lists[i].style.fontWeight="bold";
+}
